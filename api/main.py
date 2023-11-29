ef77d38ebd03d68a3648b093095d0d7ce87ee142
Kohkitos <sanzyagain@gmail.com>
2023-11-29 19:40:06 +0100

    messages function added

--- api/main.py
+++ api/main.py
@@ -11,101 +11,15 @@ db = cursor.final_project
 app = FastAPI()
 
 @app.get("/")
-def index():
+def root():
     return {"Usage": "todo"}
-
-# --- GET DICTIOS ---
-
-@app.get("/video")
-def video():
-	elements = list(db.video.find())
-	return elements
-
-@app.get("/creator")
-def creator():
-	elements = list(db.creator.find())
-	return elements
-
-@app.get("/users")
-def users():
-	elements = list(db.user.find({}, {'_id': 1, 'last_update': 1}))
-	return elements
-
-@app.get("/messages")
-def messages():
-	elements = list(db.message.find())
-	return elements
-
-@app.get("/messages/{sent}")
-def messages(sent):
-	if sent == 'positive':
-		sent = 'POS'
-	elif sent == 'neutral':
-		sent = 'NEU'
-	elif sent == 'negative':
-		sent = 'NEG'
-	else:
-		raise HTTPException(status_code=400, detail='Only "positive", "negative" or "neutral" for sentiment analysis.')
-
-	elements = list(db.message.find({'sentiment_analysis': sent}, {}))
-	return elements
-
-@app.get("/messages_by_day/{dia}")
-def messages_by_day(dia: int):
-	start_date = datetime.now().replace(day=dia, hour=0, minute=0, second=0, microsecond=0)
-	end_date = start_date.replace(day=(dia + 1))
-
-	elements = list(db.message.find({"date": {"$gte": start_date, "$lt": end_date}}, {}))
-
-	return elements
-
-# -------- REVISAR PORQUE NO FURULA POR ALGÚN MOTIVO LLORARÉ
-
-@app.get("/messages_by_day_and_sent/{param}")
-def messages_by_day_and_sent(param):
-	param = param.split('-')
-	
-	dia = int(param[0])
-	sent = param[1]
-
-	start_date = datetime.now().replace(day=dia, hour=0, minute=0, second=0, microsecond=0)
-	end_date = start_date.replace(day=(dia + 1))
-
-	if sent == 'positive':
-		sent = 'POS'
-	elif sent == 'neutral':
-		sent = 'NEU'
-	elif sent == 'negative':
-		sent = 'NEG'
-	else:
-		raise HTTPException(status_code=400, detail='Only "positive", "negative" or "neutral" for sentiment analysis.')
-
-	elements = list(db.message.find({"date": {"$gte": start_date, "$lt": end_date}, 'sentiment_analysis': sent}, {}))
-	return elements
-
-# --- GET COUNTS ---
-
-@app.get("/video_count")
-def count_total_vids():
-	elements = list(db.video.find())
-	return {"Video Count": len(elements)}
-
-@app.get("/user_count")
-def count_total_users():
-	elements = list(db.user.find())
-	return {"User count": len(elements)}
-
-@app.get("/message_count")
-def count_total_messages():
-	elements = list(db.message.find())
-	return {"Video count": len(elements)}
 
 # --- try this later ---
 
 # param == f"{sent}-{day}-{stime}-{etime} -- for everything: "POSNEGNEU-0-0-1000"
 
 def user(param):
-	params = param.split(-)
+	params = param.split('-')
 
 	users = list(db.user.find())
 
@@ -144,24 +58,32 @@ def user(param):
 	# else:
 	#     print("No se encontraron documentos para actualizar")
 
-
+@app.get("/message_{param}")
 def message(param):
-	params = param.split(-)
+	params = param.split('-')
+	parts = split_3(params[0])
 
-	if param[1] == '0':
-		messages = list(db.message.find({'sentiment_analysis': {'$in': params[0]},
-						 'timestamp': { '$gt':  params[2], '$lt': params[3]}})
+	if params[1] == '0':
+		messages = list(db.message.find({'sentiment_analysis': {'$in': parts},
+						 'timestamp': { '$gte':  int(params[2]), '$lte': int(params[3])}
+						 })
 			       )
 	else:
-		date = datetime.now().replace(day=int(param[1], hour=0, minute=0, second=0, microsecond=0)
-		messages = list(db.message.find({'sentiment_analysis': {'$in': params[0]},
+		date = datetime.now().replace(day=int(params[1]), hour=0, minute=0, second=0, microsecond=0)
+		end_date = date.replace(day = int(param[1] + 1))
+
+		messages = list(db.message.find({'sentiment_analysis': {'$in': parts},
 						'timestamp': { '$gt':  params[2], '$lt': params[3]},
-						'date': date})
+						"date": {"$gte": date, "$lt": end_date}})
 	       )
 
 		
 	result ={
-		'count': len(messages),
+		'message_count': len(messages),
 		'messages': messages
 		}
 	return result
+
+def split_3(text):
+    parts = [text[i:i + 3] for i in range(0, len(text), 3)]
+    return parts
